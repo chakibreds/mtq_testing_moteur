9da5109f1e7caa73987d4577dbee50b9b027d2fe
Kezzoul Massili <massy.kezzoul@gmail.com>
2021-12-17 17:45:59 +0100

    Duplicates reglé

--- src/plot/histogrames.py
+++ src/plot/histogrames.py
@@ -9,33 +9,41 @@ from plot import queries as q
 def hist_per_template(queries_responses_dir, queries_dir):
     # for each file in the output directory, read the queries results and put them in a list
     hist_per_template = {}
-    for file in os.listdir(queries_responses_dir):
+    files = os.listdir(queries_responses_dir)
+    actual_file = 0
+    for file in files:
         if file != "stats.csv":
+            actual_file += 1
+            print(f"Processing file ({actual_file}/{len(files)-1}): {file}")
             template_name = file.split('-')[0]
             results = pd.read_csv(queries_responses_dir + file)
-            reponses_null = 0
-            dup = q.duplicates_queries(q.parse_queries(queries_dir + template_name + '.queryset'))
 
+            reponses_null = 0
+            dup = q.parse_dup_queries(queries_dir + template_name + '.queryset')
+            #dup = q.duplicates_queries2(q.parse_queries(queries_dir + template_name + '.queryset'))
 
             # for each query result, get the number of results
             for i in range(len(results)):
                 if results.iloc[i]['Result'] == "[]":
                     # empty result
                     reponses_null += 1
+
             hist_per_template[template_name] = (reponses_null, dup)
 
     keys = sorted(hist_per_template.keys())
-    values = [hist_per_template[k] for k in keys]
+    values_null = [hist_per_template[k][0] for k in keys]
+    values_dup = [hist_per_template[k][1] for k in keys]
     x_pos = np.arange(len(keys))
 
-    plt.bar(x_pos, values)
+    plt.bar(x_pos*2, values_null)
+    plt.bar(x_pos*2+0.8, values_dup)
     
     plt.xlabel('Template name')
     plt.ylabel('Number of queries without results')
     plt.title('Histogram of queries without results')
-
+    plt.legend(['Number of queries without results', 'Number of queries with duplicates'])
     # Create names on the x-axis
-    plt.xticks(x_pos, keys, rotation=45,ha='right')
+    plt.xticks(x_pos*2+0.4, keys, rotation=45,ha='right')
     #ax.set_xticklabels(xlabels, rotation=40, ha=ha[n])
     
     # Custom the subplot layout
--- src/plot/queries.py
+++ src/plot/queries.py
@@ -1,41 +1,29 @@
-# parse a file containing a set of SPARQL Queries
-def parse_queries(file_name):
-    queries = []
+def parse_dup_queries(file_name):
+    queries = {}
+    dup = 0
     with open(file_name, 'r') as f:
         i = 0
         lines = f.readlines()
         while i < len(lines):
             line=lines[i].strip()
+            
             if line.startswith('SELECT'):
                 i += 1
             else:
+                i += 1
                 continue
             patterns = []
             while (lines[i].strip() != '}'):
-                patterns.append(lines[i].strip().split(' ')[1:2])
+                if lines[i].strip() == '':
+                    i += 1
+                    continue
+                patterns.append(tuple(lines[i].strip().split(' ')[1:3]))
                 i += 1
             i += 1
-            queries.append(patterns)
-    return queries
-
-# return true if two queries are equal
-def equal_queries(q1, q2):
-    if len(q1) != len(q2):
-        return False
-    
-    q1 = sorted(q1)
-    q2 = sorted(q2)
-    
-    for i in range(len(q1)):
-        if q1[i] != q2[i]:
-            return False
-    return True
-
-# return the number of duplicates queries in a list of queries
-def duplicates_queries(queries):
-    duplicates = 0
-    for i in range(len(queries)):
-        for j in range(i+1, len(queries)):
-            if equal_queries(queries[i], queries[j]):
-                duplicates += 1
-    return duplicates
+            patterns = tuple(sorted(patterns))
+            if patterns in queries:
+                queries[patterns] += 1
+                dup += 1
+            else:
+                queries[patterns] = 1
+    return dup
