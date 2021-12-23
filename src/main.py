04522e0fceb683f5f5d880adf66c14ad56a8534d
Kezzoul Massili <massy.kezzoul@gmail.com>
2021-12-23 22:32:20 +0100

    Avancer rapport, plan de tests et metriques

--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ from plot import histogrames as h
 if __name__ == '__main__':
     directory = '../output/test-final-qengine/'
     q_dir= '../data/queries/file-per-template-final/'
-    h.hist_per_template(directory, q_dir, option='max')
+    h.hist_per_template(directory, q_dir, option='dup')
