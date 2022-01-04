8a24e2f0abae350572808ada2f7b7a53168626a4
Kezzoul Massili <massy.kezzoul@gmail.com>
2022-01-04 15:37:22 +0100

    merging

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 QUERIES=data/queries/file-per-template-final/
-DATA=data/dataset/500k.nt
-OUTPUT=output/test-qualite-jena/
+DATA=data/dataset/1M.nt
+OUTPUT=output/test-final-qengine/
 
 PARAM=-queries $(QUERIES) -data $(DATA) -output $(OUTPUT) -export_query_results
 
---
+++ src/test-qualite.sh
@@ -0,0 +1,59 @@
+# Ce script réalise le protocol de test de qualité
+
+DATASET="../data/2M.nt"
+QUERIES="../data/queries/file-per-template-final/"
+
+PY="protocole/qualite.py"
+JAR="../rdfengine.jar"
+
+OUTPUT="../output/test-qualite/"
+O_QENGINE=$OUTPUT"qengine/"
+O_JENA=$OUTPUT"jena/"
+
+CMDJAR="java -jar "$JAR" -export_query_results -queries "$QUERIES" -data "$DATA" -output "
+
+if [ ! -d $QUERIES ] || [ ! -d $TEMPLATE_DIR ]; then
+   echo "Error: directory not found"
+   exit 1
+fi
+
+if [ ! -f $DATASET ] || [ ! -f $PY ] || [ ! -f $JAR ]; then
+   echo "Error: file not found"
+   #exit 1
+fi
+
+if [ ! -d $OUTPUT ]; then
+   echo "Creating directory $OUTPUT"
+   mkdir $OUTPUT
+   echo "Creating directory $O_QENGINE"
+   mkdir $O_QENGINE
+   echo "Creating directory $O_JENA"
+   mkdir $O_JENA
+else
+    echo -e "Directory $OUTPUT already exists\nDo you want to overwrite ? (any key to continue / CTRL-C to exit)"
+    read $x
+    echo -e "Overwriting..."
+    if [ ! -d $O_QENGINE ]; then
+        echo "Creating directory $O_QENGINE"
+        mkdir $O_QENGINE
+    fi
+    if [ ! -d $O_JENA ]; then
+        echo "Creating directory $O_JENA"
+        mkdir $O_JENA
+    fi
+fi
+
+echo "Running qengine on dataset "$DATASET" and queries "$QUERIES
+echo "Output will be in "$O_QENGINE
+
+$CMDJAR $O_QENGINE > /dev/null
+
+echo "Running jena on dataset "$DATASET" and queries "$QUERIES
+echo "Output will be in "$O_JENA
+
+$CMDJAR $O_JENA -jena > /dev/null
+
+echo "Runninge "$PY
+
+echo python $PY $O_QENGINE $O_JENA
+
